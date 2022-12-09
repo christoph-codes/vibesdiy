75544df4c9d4da1c0605fa2262b24321ebdb76fb
christoph-codes <tkcwebdev@gmail.com>
2022-12-09 15:33:05 -0800

    15: Create components test playground page

---
+++ site/pages/components.tsx
@@ -0,0 +1,13 @@
+import Title from "../components/Title";
+
+const Components = () => {
+	return (
+		<>
+			<Title header="h2" eyebrow="primary" textAlign="center">
+				Hey Gurl, Whats Up
+			</Title>
+		</>
+	);
+};
+
+export default Components;
