9dc121ee3456f4a0ae5a276b8e88f264e115981c
Christopher Jones <tkcwebdev@gmail.com>
2022-12-08 18:38:56 -0800

    main: Potential fix for the mobile scroll

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+	const vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty("--vh", `${vh}px`);
 	return <Component {...pageProps} />;
 }
