e0c077268cd053943e94ad6a714818544a481b84
Christopher Jones <tkcwebdev@gmail.com>
2022-12-08 17:54:38 -0800

    main: Remove drop shadow from logo and add with styles

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
 	return (
@@ -7,15 +8,21 @@ export default function Home() {
 			<Head>
 				<title>Vibes DIY Studio</title>
 				<meta
-					name='description'
-					content='Vibes DIY Studio » The new creative place coming to Centennial Hills in Las Vegas'
+					name="description"
+					content="Vibes DIY Studio » The new creative place coming to Centennial Hills in Las Vegas"
 				/>
-				<link rel='icon' href='/favicon.ico' />
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main className={styles.ComingSoon}>
 				<article>
 					<h1>Vibes DIY</h1>
+					<Image
+						width={500}
+						height={200}
+						src="/logo_white.svg"
+						alt="Vibes DIY Studio Logo"
+					/>
 					<p>Website Coming Soon</p>
 				</article>
 			</main>
