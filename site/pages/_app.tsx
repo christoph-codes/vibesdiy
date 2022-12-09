aee592d6be94313a25f7f739cd4a38f1c04d81ac
christoph-codes <tkcwebdev@gmail.com>
2022-12-09 13:51:33 -0800

    12: Install chakra theme

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "../styles/globals.scss";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import theme from "../styles/theme";
+import "../styles/globals.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
+};
+
+export default App;
---
+++ site/styles/theme.tsx
@@ -0,0 +1,39 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+	brand: {
+		primary: {
+			default: "#E31E26",
+			hover: "#FF545B",
+		},
+		secondary: {
+			default: "279546",
+			hover: "40BC63",
+		},
+		tertiary: {
+			default: "#FCD504",
+			hover: "#FFE55C",
+		},
+		quad: {
+			default: "#3C53A4",
+			hover: "#4F70E2",
+		},
+		black: {
+			default: "#010101",
+			hover: "#1A1A1A",
+		},
+		white: {
+			default: "#FFFFFF",
+			hover: "#DEDEDE",
+		},
+	},
+};
+const fonts = {
+	body: "system-ui, sans-serif",
+	heading: "Montserrat, sans-serif",
+	mono: "Menlo, monospace",
+};
+
+const theme = extendTheme({ colors, fonts });
+
+export default theme;
