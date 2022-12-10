5288bce4ac6708d995d9eb9ff95e6ac63d81b006
christoph-codes <tkcwebdev@gmail.com>
2022-12-09 16:14:53 -0800

    15: More tests for titles

--- site/pages/components.tsx
+++ site/pages/components.tsx
@@ -3,7 +3,13 @@ import Title from "../components/Title";
 const Components = () => {
 	return (
 		<>
-			<Title header="h2" eyebrow="primary" textAlign="center">
+			<Title header="h1" eyebrow="primary" textAlign="left">
+				Hey Gurl, Whats Up
+			</Title>
+			<Title header="h2" eyebrow="secondary" textAlign="center">
+				Hey Gurl, Whats Up
+			</Title>
+			<Title header="h3" eyebrow="tertiary" textAlign="center">
 				Hey Gurl, Whats Up
 			</Title>
 		</>
