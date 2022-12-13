ca4484a3394906979bef8bb255ed44589bf8f221
christoph-codes <tkcwebdev@gmail.com>
2022-12-12 17:32:22 -0800

    5: Test Navlink component on test page and adjust for spacing

--- site/pages/components.tsx
+++ site/pages/components.tsx
@@ -2,6 +2,7 @@ import Section from "../components/Section";
 import Title from "../components/Title";
 import Logo from "../components/Logo";
 import PageTemplate from "../templates/Page";
+import NavLink from "../components/NavLink";
 
 const Components = () => {
 	return (
@@ -13,6 +14,18 @@ const Components = () => {
 					<Logo variant="light" />
 					<Logo variant="lightNoTag" />
 					<Logo variant="icon" />
+					<NavLink
+						dropdownLinks={[
+							{ label: "Hello", path: "/hello" },
+							{ label: "Hello2", path: "/hello2" },
+						]}
+					>
+						Page
+					</NavLink>
+					<NavLink href="">Page 2</NavLink>
+					<NavLink href="/three">Page 3</NavLink>
+					<NavLink href="/four">Page 4</NavLink>
+					<NavLink href="/five">Page 5</NavLink>
 					<Title header="h1" eyebrow="primary" textAlign="left">
 						Hey Gurl, Whats Up
 					</Title>
