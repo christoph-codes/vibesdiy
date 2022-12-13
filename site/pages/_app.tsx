917235530b58a75d92e0790f924640ec187ee64b
christoph-codes <tkcwebdev@gmail.com>
2022-12-13 10:19:52 -0800

    3: Create sidenav provider for opening and closing navigation on mobile

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -2,11 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../styles/theme";
 import "../styles/globals.scss";
+import SideNavProvider from "../providers/SidenavProvider";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
+			<SideNavProvider>
+				<Component {...pageProps} />
+			</SideNavProvider>
 		</ChakraProvider>
 	);
 };
