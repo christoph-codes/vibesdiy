2dcc2563a331856c8c0db87d1e65825bd44f69dd
christoph-codes <tkcwebdev@gmail.com>
2022-12-13 10:21:08 -0800

    3: Test navigation component and reduce size of logos for seeing all components in teest page

--- site/pages/components.tsx
+++ site/pages/components.tsx
@@ -5,11 +5,15 @@ import PageTemplate from "../templates/Page";
 import NavLink from "../components/NavLink";
 import Button from "../components/Button";
 import { Flex } from "@chakra-ui/react";
+import Navigation from "../components/Navigation";
+import { navigationLinks } from "../utils/navLinks";
 
 const Components = () => {
 	return (
 		<>
 			<PageTemplate>
+				<Title h4>Navigation</Title>
+				<Navigation links={navigationLinks} />
 				<Section>
 					<Title h4>Button</Title>
 					<Flex>
@@ -25,11 +29,13 @@ const Components = () => {
 						<Button variant="white">Hello</Button>
 					</Flex>
 					<Title h4>Logo</Title>
-					<Logo />
-					<Logo variant="noTag" />
-					<Logo variant="light" />
-					<Logo variant="lightNoTag" />
-					<Logo variant="icon" />
+					<Flex alignItems="center" columnGap="5">
+						<Logo />
+						<Logo variant="noTag" />
+						<Logo variant="light" />
+						<Logo variant="lightNoTag" />
+						<Logo variant="icon" />
+					</Flex>
 					<Title h4>NavLink</Title>
 					<NavLink
 						dropdownLinks={[
