0fe21790cccb87afe9422cca74a548ecd8b49c4c
Christopher Jones <tkcwebdev@gmail.com>
2022-12-27 00:57:09 -0800

    25: Create home page accordion content file. To be updated

---
+++ site/utils/content/homeFaqs.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { IAccordionItem } from '../../components/Accordion';
+
+const homeFaqs: IAccordionItem[] = [
+	{
+		title: 'Question for the people in the back',
+		content: (
+			<p>
+				{`Lorem Ipsum is simply dummy text of the
+                printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled
+                it to make a type specimen book. It has
+                survived not only five centuries, but also
+                the leap into electronic typesetting,
+                remaining essentially unchanged. It was
+                popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop
+                publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum`}
+			</p>
+		),
+	},
+	{
+		title: 'Question for the people in the back back',
+		content: (
+			<p>
+				This is the answer to the queestion for the people in the back
+				back.
+			</p>
+		),
+	},
+	{
+		title: 'Question for the people by the people #3?',
+		content: (
+			<p>
+				This is the #3 answer to the queestion for the people in the
+				back back back. Come in and get your special tufting material
+				today!
+			</p>
+		),
+	},
+	{
+		title: 'Question for the people in the back',
+		content: (
+			<p>
+				{`Lorem Ipsum is simply dummy text of the
+                printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled
+                it to make a type specimen book. It has
+                survived not only five centuries, but also
+                the leap into electronic typesetting,
+                remaining essentially unchanged. It was
+                popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum
+                passages, and more recently with desktop
+                publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum`}
+			</p>
+		),
+	},
+];
+
+export default homeFaqs;
