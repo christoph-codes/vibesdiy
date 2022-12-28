f2de7d407389a57542fbb32b3e8fd1465ed28a72
Christopher Jones <tkcwebdev@gmail.com>
2022-12-28 14:37:02 -0800

    26: Add missing href attribute for navigation navlinks

--- site/components/Navigation/index.tsx
+++ site/components/Navigation/index.tsx
@@ -1,17 +1,17 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
-import { Box, Flex } from "@chakra-ui/react";
-import Link from "next/link";
-import { useSideNav } from "../../providers/SidenavProvider";
-import Container from "../Container";
-import Logo from "../Logo";
-import NavLink, { INavLinkProps, ILink } from "../NavLink";
-import styles from "./Navigation.module.scss";
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Box, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+import { useSideNav } from '../../providers/SidenavProvider';
+import Container from '../Container';
+import Logo from '../Logo';
+import NavLink, { INavLinkProps, ILink } from '../NavLink';
+import styles from './Navigation.module.scss';
 
 export interface INavigationProps {
 	className?: string;
 	links?: {
 		link: ILink;
-		dropdown?: INavLinkProps["dropdownLinks"];
+		dropdown?: INavLinkProps['dropdownLinks'];
 		cta?: boolean;
 	}[];
 }
@@ -19,9 +19,9 @@ export interface INavigationProps {
 const Navigation = ({ links, className }: INavigationProps) => {
 	const { toggleSideNav } = useSideNav();
 	return (
-		<Box as="nav" className={`${styles.Navigation} ${className || ""}`}>
+		<Box as='nav' className={`${styles.Navigation} ${className || ''}`}>
 			<Container className={styles.Navigation__container}>
-				<Link className={styles.Navigation__logoContainer} href="/">
+				<Link className={styles.Navigation__logoContainer} href='/'>
 					<Logo className={styles.Navigation__logo} />
 				</Link>
 				<ul className={styles.Navigation__nav}>
@@ -31,6 +31,8 @@ const Navigation = ({ links, className }: INavigationProps) => {
 								key={index}
 								dropdownLinks={item.dropdown}
 								cta={item.cta}
+								href={item.link.path}
+								target={item.link.target}
 							>
 								{item.link.label}
 							</NavLink>
@@ -40,8 +42,8 @@ const Navigation = ({ links, className }: INavigationProps) => {
 				<Box className={styles.Navigation__mobileNav}>
 					<NavLink
 						className={styles.Navigation__mobileCTA}
-						target="_blank"
-						href="https://squareup.com/appointments/buyer/widget/yow3ms7055ip18/L9QAGAMTXZ4C7"
+						target='_blank'
+						href='https://squareup.com/appointments/buyer/widget/yow3ms7055ip18/L9QAGAMTXZ4C7'
 						cta
 					>
 						Book Now
