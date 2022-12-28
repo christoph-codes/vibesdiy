c70c001f0a2849fcd6f0838e848edec7178f1c9b
Christopher Jones <tkcwebdev@gmail.com>
2022-12-28 14:37:18 -0800

    26: Add seo image to coming soon page

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Text } from "@chakra-ui/react";
-import Head from "next/head";
-import Logo from "../components/Logo";
-import styles from "../styles/Home.module.scss";
+import { Text } from '@chakra-ui/react';
+import Head from 'next/head';
+import Logo from '../components/Logo';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
 	return (
@@ -9,17 +9,18 @@ export default function Home() {
 			<Head>
 				<title>Vibes DIY Studio</title>
 				<meta
-					name="description"
-					content="Vibes DIY Studio » The new creative place coming to Centennial Hills in Las Vegas"
+					name='description'
+					content='Vibes DIY Studio » The new creative place coming to Centennial Hills in Las Vegas'
 				/>
-				<link rel="icon" href="/favicon.ico" />
+				<meta content='/vibes_seo.png' property='og:image' />
+				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
 			<main className={styles.ComingSoon}>
 				<article>
 					<h1>Vibes DIY</h1>
-					<Logo variant="white" />
-					<Text textColor="brand.white.default">
+					<Logo variant='white' />
+					<Text textColor='brand.white.default'>
 						Website Coming Soon
 					</Text>
 				</article>
