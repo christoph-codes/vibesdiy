9333fcc7a6eedc7c09f0af324eeae3b90628f766
Christopher Jones <tkcwebdev@gmail.com>
2023-01-01 23:36:25 -0800

    main: Update book page file structure to include scss file for styling iframe. WIP

--- site/pages/book/index.tsx
+++ site/pages/book/index.tsx
@@ -1,17 +1,22 @@
 import Script from 'next/script';
-import Section from '../components/Section';
-import PageTemplate from '../templates/Page';
+import Section from '../../components/Section';
+import PageTemplate from '../../templates/Page';
+import styles from './Book.module.scss';
 
 const Book = () => {
 	return (
 		<>
 			<PageTemplate metaTitle='Book A Session with Vibes DIY Studio Today'>
-				<Section title='Book A Session Today'>
+				<Section
+					title='Book A Session Today'
+					containerClass={styles.Book__container}
+				>
 					<iframe
 						src='https://app.squarespacescheduling.com/schedule.php?owner=28028184'
 						title='Schedule Appointment'
 						width='100%'
-						height='800'
+						height='100%'
+						seamless
 					></iframe>
 					<Script
 						src='https://embed.acuityscheduling.com/js/embed.js'
