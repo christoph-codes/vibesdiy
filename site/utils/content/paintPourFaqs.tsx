8da5b95edc1eb9ead083981b93ca4c77645da0b3
christoph-codes <tkcwebdev@gmail.com>
2023-01-08 21:56:13 -0800

    57: Create separate service faqs content

---
+++ site/utils/content/paintPourFaqs.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import { IAccordionItem } from "../../components/Accordion";
+
+const paintPourFaqs: IAccordionItem[] = [
+	{
+		title: "What is paint pouring?",
+		content: (
+			<p>
+				Paint Pouring is an activity using multiple cups of paint and
+				pouring it over a preselected figurine to create your own unique
+				and custom design and look!
+			</p>
+		),
+	},
+	{
+		title: "Is paint pouring difficult?",
+		content: (
+			<p>
+				It’s not as intimidating as it looks. If you have ever poured a
+				drink you can pour paint. Just make sure you keep the paint on
+				the provided tray and to completely cover your figurine and you
+				are good! We’ll be there to guide you along the way, but what’s
+				most important is to relax, and get lost in your art while
+				HAVING FUN!
+			</p>
+		),
+	},
+	{
+		title: "Does my deposit go toward my final payment?",
+		content: (
+			<p>
+				Yes, when you come in to your appointment you will pay the
+				remaining balance of the craft of your choice.
+			</p>
+		),
+	},
+	{
+		title: "How long does paint pouring take?",
+		content: (
+			<p>
+				Depending on the figurine you choose and your creative juices,
+				paint pours could take anywhere between 30 minutes to an hour.
+			</p>
+		),
+	},
+	{
+		title: "What if I don’t finish my paint pour?",
+		content: (
+			<p>
+				We’ll work with everyone from design selection to ensure that
+				you have as much time as possible to complete your paint design.
+				If you still can’t finish, we can schedule you to return to
+				finish for an additional studio fee.
+			</p>
+		),
+	},
+	{
+		title: "Are food and drinks allowed?",
+		content: (
+			<p>
+				We do offer complimentary water and a small snack selection for
+				purchasing. There are food places in the area that you can enjoy
+				prior to your appointment.
+			</p>
+		),
+	},
+	{
+		title: "Can I bring a guest if they are not tufting or painting?",
+		content: (
+			<p>
+				Our stations are designed to fit you and the supplies you’ll
+				need. For your own safety and comfort, only one person per
+				station.
+			</p>
+		),
+	},
+	{
+		title: "Do I need to bring anything?",
+		content: (
+			<p>
+				Just yourself! :) We will supply you with everything you need
+				for your craft.
+			</p>
+		),
+	},
+	{
+		title: "What do I wear?",
+		content: (
+			<p>
+				Whatever makes you the most comfortable and will allow you to
+				move freely. If your hair is down, please bring a hair tie to
+				pull it back.
+			</p>
+		),
+	},
+	{
+		title: "What if I am running late?",
+		content: (
+			<p>
+				We ask you to call our studio number so we can adjust as needed.
+				If you arrive more than 15 minutes late, we will have to give up
+				your time. We can not ensure the quality and the completion of
+				all rugs if your time is cut too short.
+			</p>
+		),
+	},
+	{
+		title: "What is your Refund, Cancellation and Rescheduling Policy",
+		content: (
+			<p>
+				All sales are final. Unfortunately no refunds will be issued. If
+				you need to reschedule we ask that you contact us 48 hours in
+				advance. 702-395-3349
+			</p>
+		),
+	},
+	{
+		title: "If I cannot make my reservation, can I send someone in my place?",
+		content: (
+			<p>
+				Yes, give us a call to change reservation information.
+				702-395-3349 (Deposit is non-refundable)
+			</p>
+		),
+	},
+	{
+		title: "Can I book a private or group class?",
+		content: (
+			<p>
+				Yes! We welcome all parties and events. Let us help arrange that
+				but contacting us by phone (702-395-3349) or email
+				groups@vibesdiystudio.com
+			</p>
+		),
+	},
+];
+
+export default paintPourFaqs;
