1f7fda68ee01280b20a65b8ac743247d66fcd550
christoph-codes <tkcwebdev@gmail.com>
2023-01-08 19:50:36 -0800

    57: Update about page with new content. Resolves issue #58

--- site/pages/about.tsx
+++ site/pages/about.tsx
@@ -1,85 +1,62 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
-import Hero from '../components/Hero';
-import Section from '../components/Section';
-import Title from '../components/Title';
-import PageTemplate from '../templates/Page';
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import Hero from "../components/Hero";
+import Section from "../components/Section";
+import Title from "../components/Title";
+import PageTemplate from "../templates/Page";
 
 const About = () => {
 	return (
 		<PageTemplate
-			metaTitle='About'
-			metaDescription='Learn About Vibes DIY Studio and how they came to be in the top creative studio in Las Vegas'
+			metaTitle="About"
+			metaDescription="Learn About Vibes DIY Studio and how they came to be in the top creative studio in Las Vegas"
 		>
-			<Hero title='About' bgImg='/about_bg.png' />
+			<Hero title="About" bgImg="/about_bg.png" />
 			<Section>
 				<Flex
-					justifyContent='space-between'
+					justifyContent="space-between"
 					columnGap={24}
-					flexDirection={{ base: 'column', md: 'row' }}
+					flexDirection={{ base: "column", md: "row" }}
 				>
-					<Box width={{ base: '100%', md: '50%' }}>
+					<Box width={{ base: "100%", md: "50%" }}>
 						<Image
-							src='/vibes_seo.png'
-							alt='Image of the owners of Vibes DIY Studio'
+							src="/about_team.png"
+							alt="Image of the owners of Vibes DIY Studio"
 							flexGrow={1}
 							flexShrink={1}
 							marginBottom={{ base: 8, md: 0 }}
-							height='auto'
+							height="auto"
 						/>
-						<Text color='red' fontWeight='bold'>
-							Replace with actual image
-						</Text>
 					</Box>
-					<Box width={{ base: '100%', md: '50%' }}>
+					<Box width={{ base: "100%", md: "50%" }}>
 						<Title h2>About Vibes DIY Studio</Title>
-						<Text>
-							{`Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry. Lorem Ipsum has been the
-							industry's standard dummy text ever since the 1500s,
-							when an unknown printer took a galley of type and
-							scrambled it to make a type specimen book. It has
-							survived not only five centuries, but also the leap
-							into electronic typesetting, remaining essentially
-							unchanged. It was popularised in the 1960s with the
-							release of Letraset sheets containing Lorem Ipsum
-							passages, and more recently with desktop publishing
-							software like Aldus PageMaker including versions of
-							Lorem Ipsum.`}
+						<Text lineHeight="7">
+							Like lots of others during the 2020 pandemic, I
+							experienced many positives and some negatives in
+							life. After many years and longer days working
+							remote at a desk and in front of the computer, lots
+							of medical testing, I learned sitting 8+ hours a day
+							until retirement was not going to be any good for
+							me. I decided that in 2022 I was going to set a goal
+							and depend on prayer to guide me to do something
+							different. I get a creative itch regularly and want
+							to do some type of DIY project. I always thought if
+							I could do what I wanted and be happy I would be
+							able to create and see others happy as well.
 						</Text>
-						<Title h3>History</Title>
-						<Text>
-							{`It is a long established fact that a reader will be
-							distracted by the readable content of a page when
-							looking at its layout. The point of using Lorem
-							Ipsum is that it has a more-or-less normal
-							distribution of letters, as opposed to using
-							'Content here, content here', making it look like
-							readable English. Many desktop publishing packages
-							and web page editors now use Lorem Ipsum as their
-							default model text, and a search for 'lorem ipsum'
-							will uncover many web sites still in their infancy.
-							Various versions have evolved over the years,
-							sometimes by accident, sometimes on purpose
-							(injected humour and the like).`}
-						</Text>
-						<Title h3>Our Vision</Title>
-						<Text>
-							{`Contrary to popular belief, Lorem Ipsum is not
-							simply random text. It has roots in a piece of
-							classical Latin literature from 45 BC, making it
-							over 2000 years old. Richard McClintock, a Latin
-							professor at Hampden-Sydney College in Virginia,
-							looked up one of the more obscure Latin words,
-							consectetur, from a Lorem Ipsum passage, and going
-							through the cites of the word in classical
-							literature, discovered the undoubtable source. Lorem
-							Ipsum comes from sections 1.10.32 and 1.10.33 of "de
-							Finibus Bonorum et Malorum" (The Extremes of Good
-							and Evil) by Cicero, written in 45 BC. This book is
-							a treatise on the theory of ethics, very popular
-							during the Renaissance. The first line of Lorem
-							Ipsum, "Lorem ipsum dolor sit amet..", comes from a
-							line in section 1.10.32.`}
+						<Text lineHeight="7">
+							Once deciding to take the leap of faith and
+							brainstorming with my daughter we came up with
+							Vibes. After watching many TikTok videos, we tried
+							our hand at rug tufting and haven’t looked back.
+							What better to have a creative space for people to
+							come and be happy and express their creative vibe.
+							My daughter Cooper and her husband CJ are creatives
+							themselves. Combining Kenzie Rae and to be able to
+							create with them has been a joy. Anything for the
+							grand babies, as you may see them growing up in the
+							studio. Can’t wait to see all our friends and family
+							come through and all of you we don’t even know yet.
+							Cheers to 2023. Happy New Year.
 						</Text>
 					</Box>
 				</Flex>
