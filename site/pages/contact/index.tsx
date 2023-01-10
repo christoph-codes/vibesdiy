63c92000f0eba2988e9dcfc22e15ae322b2fbb4d
christoph-codes <tkcwebdev@gmail.com>
2023-01-09 20:40:23 -0800

    62: Update address on the contact page

--- site/pages/contact/index.tsx
+++ site/pages/contact/index.tsx
@@ -1,15 +1,15 @@
-import { Box, Flex, Icon, Text } from '@chakra-ui/react';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-import { useState } from 'react';
-import Button from '../../components/Button';
-import Eyebrow from '../../components/Eyebrow';
-import Form from '../../components/Form';
-import Hero from '../../components/Hero';
-import Section from '../../components/Section';
-import Title from '../../components/Title';
-import PageTemplate from '../../templates/Page';
-import Link from 'next/link';
-import { facebookLink, instagramLink, twitterLink } from '../../utils/navLinks';
+import { Box, Flex, Icon, Text } from "@chakra-ui/react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { useState } from "react";
+import Button from "../../components/Button";
+import Eyebrow from "../../components/Eyebrow";
+import Form from "../../components/Form";
+import Hero from "../../components/Hero";
+import Section from "../../components/Section";
+import Title from "../../components/Title";
+import PageTemplate from "../../templates/Page";
+import Link from "next/link";
+import { facebookLink, instagramLink, twitterLink } from "../../utils/navLinks";
 
 const Contact = () => {
 	const [successfulSubmission, setSuccessfulSubmission] =
@@ -19,21 +19,21 @@ const Contact = () => {
 	};
 	return (
 		<PageTemplate
-			metaTitle='Contact Vibes DIY Studio Today'
-			metaDescription='Contact Vibes DIY Studio, the Premiere Creative Space located in the Northwest Las Vegas area.'
+			metaTitle="Contact Vibes DIY Studio Today"
+			metaDescription="Contact Vibes DIY Studio, the Premiere Creative Space located in the Northwest Las Vegas area."
 		>
-			<Hero title='Contact' bgImg='/location1.png' />
+			<Hero title="Contact" bgImg="/location1.png" />
 			<Section>
 				<Flex
-					justifyContent='space-between'
+					justifyContent="space-between"
 					columnGap={48}
-					flexDirection={{ base: 'column-reverse', md: 'row' }}
+					flexDirection={{ base: "column-reverse", md: "row" }}
 				>
 					<Box
-						width={{ base: '100%', md: '50%' }}
+						width={{ base: "100%", md: "50%" }}
 						marginTop={{ base: 8, md: 0 }}
 					>
-						<Title h3 eyebrow='primary'>
+						<Title h3 eyebrow="primary">
 							Get in touch with us today!
 						</Title>
 						<Text>
@@ -43,75 +43,75 @@ const Contact = () => {
 						{!successfulSubmission ? (
 							<Form
 								onSubmit={submitContact}
-								formName='contact'
+								formName="contact"
 								submitButton={{
-									children: 'Submit',
+									children: "Submit",
 								}}
 								inputs={[
 									{
-										type: 'text',
-										name: 'fname',
-										placeholder: 'John',
-										label: 'First Name',
+										type: "text",
+										name: "fname",
+										placeholder: "John",
+										label: "First Name",
 										required: true, //@ts-ignore
-										validation: ['REQUIRED'],
+										validation: ["REQUIRED"],
 									},
 									{
-										type: 'text',
-										name: 'lname',
-										placeholder: 'Doe',
-										label: 'Last Name',
+										type: "text",
+										name: "lname",
+										placeholder: "Doe",
+										label: "Last Name",
 										required: true, //@ts-ignore
-										validation: ['REQUIRED'],
+										validation: ["REQUIRED"],
 									},
 									{
-										type: 'email',
-										name: 'email',
-										placeholder: 'john@doe.com',
-										label: 'Email',
+										type: "email",
+										name: "email",
+										placeholder: "john@doe.com",
+										label: "Email",
 										required: true, //@ts-ignore
-										validation: ['REQUIRED', 'EMAIL'],
+										validation: ["REQUIRED", "EMAIL"],
 									},
 									{
-										type: 'textarea',
-										name: 'message',
+										type: "textarea",
+										name: "message",
 										placeholder:
-											'Let us know how we are doing!',
-										label: 'Message',
+											"Let us know how we are doing!",
+										label: "Message",
 										required: true, //@ts-ignore
-										validation: ['REQUIRED'],
+										validation: ["REQUIRED"],
 									},
 								]}
 							/>
 						) : (
 							<Flex
-								borderTop='4px'
+								borderTop="4px"
 								paddingTop={8}
-								flexDirection='column'
-								alignItems='center'
+								flexDirection="column"
+								alignItems="center"
 							>
 								<Title
 									h4
-									textAlign='center'
-									color='brand.secondary.default'
+									textAlign="center"
+									color="brand.secondary.default"
 								>
 									Thank you so much!
 								</Title>
-								<Text textAlign='center'>
+								<Text textAlign="center">
 									Someone from our team will be getting in
 									touch with you very soon.
 								</Text>
-								<Button variant='primary-outline' href='/'>
+								<Button variant="primary-outline" href="/">
 									Go Home
 								</Button>
 							</Flex>
 						)}
 					</Box>
-					<Box width={{ base: '100%', md: '50%' }}>
-						<Eyebrow color='primary' />
+					<Box width={{ base: "100%", md: "50%" }}>
+						<Eyebrow color="primary" />
 						<Title h5>Address:</Title>
 						<Text>
-							7557 Norman Rockwell Ln, Suite 120
+							7575 Norman Rockwell Ln, Suite 120
 							<br />
 							Las Vegas, NV, 89143
 							<br />
@@ -130,34 +130,34 @@ const Contact = () => {
 						<Flex gap={4}>
 							<Link href={facebookLink}>
 								<Icon
-									aria-label='Visit Vibes DIY Studio on Facebook'
+									aria-label="Visit Vibes DIY Studio on Facebook"
 									as={FaFacebook}
 									boxSize={8}
-									color='brand.primary.default'
+									color="brand.primary.default"
 									_hover={{
-										color: 'brand.primary.hover',
+										color: "brand.primary.hover",
 									}}
 								/>
 							</Link>
 							<Link href={instagramLink}>
 								<Icon
-									aria-label='Visit Vibes DIY Studio on Facebook'
+									aria-label="Visit Vibes DIY Studio on Facebook"
 									as={FaInstagram}
 									boxSize={8}
-									color='brand.primary.default'
+									color="brand.primary.default"
 									_hover={{
-										color: 'brand.primary.hover',
+										color: "brand.primary.hover",
 									}}
 								/>
 							</Link>
 							<Link href={twitterLink}>
 								<Icon
-									aria-label='Visit Vibes DIY Studio on Facebook'
+									aria-label="Visit Vibes DIY Studio on Facebook"
 									as={FaTwitter}
 									boxSize={8}
-									color='brand.primary.default'
+									color="brand.primary.default"
 									_hover={{
-										color: 'brand.primary.hover',
+										color: "brand.primary.hover",
 									}}
 								/>
 							</Link>
