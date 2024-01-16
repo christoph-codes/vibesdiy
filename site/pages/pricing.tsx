c225d579ba0988c8160147d92d52757535ce84c4
christoph-codes <tkcwebdev@gmail.com>
2024-01-16 00:34:47 -0800

    78: Update pricing page with new pricing sheet for Jan 15, 2024. Resolves #78

--- site/pages/pricing.tsx
+++ site/pages/pricing.tsx
@@ -13,12 +13,12 @@ const Pricing = () => {
 			<Hero title="Price List" bgImg="/about_bg.png" />
 			<Section>
 				<Text textAlign="center">
-					Our pricing structure is updated regularly and showcases all
-					of prices of the items purchased with our services.
+					Our pricing structure is updated regularly and showcases all of prices
+					of the items purchased with our services.
 				</Text>
 				<Image
 					width="100%"
-					src="/pricing_092123.jpg"
+					src="/pricing_011524.jpg"
 					alt="Vibes DIY Pricelist"
 				/>
 			</Section>
